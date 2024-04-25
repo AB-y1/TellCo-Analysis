23d063a51827ba93e62284cdb2e2d1086c47f627
AB-y1 <abdalrhmanalsh6@gmail.com>
2024-04-25 23:24:20 +0200

    task 2.1

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+
+import pandas as pd
+import numpy as np
+from IPython.display import Image
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_hist(df:pd.DataFrame, column:str, color:str)->None:
+    # plt.figure(figsize=(15, 10))
+    # fig, ax = plt.subplots(1, figsize=(12, 7))
+    sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
+    plt.title(f'Distribution of {column}', size=20, fontweight='bold')
+    plt.show()
+
+def plot_box(df:pd.DataFrame, x_col:str, title:str) -> None:
+    plt.figure(figsize=(12, 7))
+    sns.boxplot(data = df, x=x_col)
+    plt.title(title, size=20)
+    plt.xticks(rotation=75, fontsize=14)
+    plt.show()
